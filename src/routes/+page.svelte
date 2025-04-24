b85f56dd08bdb82bb4baa33b0cbbc17087bbc23f
notLeeter <67231490+notLeeter@users.noreply.github.com>
2025-04-24 12:11:25 +0200

    implement websocket example

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,24 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://svelte.dev/docs/kit">svelte.dev/docs/kit</a> to read the documentation</p>
+<script>
+    import { onMount } from 'svelte';
+    let rfidMessage = 'Waiting for tag...';
+
+    onMount(() => {
+        const socket = new WebSocket('ws://localhost:9000');
+
+        socket.addEventListener('message', (event) => {
+            const data = event.data;
+            rfidMessage = data;
+        });
+
+        socket.addEventListener('open', () => {
+            console.log('Connected to WebSocket server');
+        });
+
+        socket.addEventListener('error', (err) => {
+            console.error('WebSocket error:', err);
+        });
+    });
+</script>
+
+<h1>RFID Reader</h1>
+<p>{rfidMessage}</p>
